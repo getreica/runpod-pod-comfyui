9708e71700b1a72d8234abbe399256b535138070
Alex Genovese <423745+alexgenovese@users.noreply.github.com>
2025-01-14 11:16:50 +0100

    Update push-to-docker-hub.sh

--- push-to-docker-hub.sh
+++ push-to-docker-hub.sh
@@ -1,9 +1,33 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-TAG_NAME=1.0.4
+# Stop on error
+set -e
 
-# build with latest tag
-docker build -t alexgenovese/runpod-pod-comfyui:${TAG_NAME} --platform linux/amd64 .
+# Variables
+IMAGE_NAME="runpod-pod-comfyui"
+TAG="1.1.5"
+DOCKER_HUB_USERNAME="alexgenovese"
 
-# Publish on hub 
-docker push alexgenovese/runpod-pod-comfyui:${TAG_NAME}
+# Ensure the script is run with the correct number of arguments
+if [ "$#" -eq 2 ]; then
+    IMAGE_NAME="$1"
+    TAG="$2"
+fi
+
+# Check if already logged into Docker Hub
+if docker info | grep -q "Username: $DOCKER_HUB_USERNAME"; then
+    echo "Already logged into Docker Hub as $DOCKER_HUB_USERNAME."
+else
+    echo "Logging into Docker Hub..."
+    docker login -u "$DOCKER_HUB_USERNAME"
+fi
+
+# Build the Docker image
+echo "Building the Docker image: $IMAGE_NAME:$TAG"
+docker build -t "$DOCKER_HUB_USERNAME/$IMAGE_NAME:$TAG" --platform linux/amd64 .
+
+# Push the Docker image to Docker Hub
+echo "Pushing the Docker image to Docker Hub"
+docker push "$DOCKER_HUB_USERNAME/$IMAGE_NAME:$TAG"
+
+echo "Docker image successfully built and pushed!"
