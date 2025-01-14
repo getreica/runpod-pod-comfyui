80b44540ce4412125182d11ad4adc82727e6efa7
Alex Genovese <423745+alexgenovese@users.noreply.github.com>
2025-01-14 11:16:47 +0100

    fix wrong path

--- 2-comfyui-on-workspace.sh
+++ 2-comfyui-on-workspace.sh
@@ -9,8 +9,8 @@ comfy node install https://github.com/pythongosssss/ComfyUI-Custom-Scripts
 
 # Start download nodes in /workspace/custom_nodes 
 # Download all custom nodes in /workspace/custom_nodes
-# chmod 755 /download-custom-nodes.sh
-# /download-custom-nodes.sh
+chmod 755 /download-custom-nodes.sh
+/download-custom-nodes.sh
 
 # Update default settings
 mv /comfy.settings.json /comfyui/comfy.settings.json
@@ -18,4 +18,4 @@ python3 /replaceDefaultGraph.py
 
 # remove models
 rm -rf /workspace/ComfyUI/models
-ln -s /workspace/models /workspace/ComfyUI/models
+ln -s /workspace/models /workspace/comfyui/models
