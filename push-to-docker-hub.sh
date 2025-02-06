7df4363d0e9a5f3187a3e9ff1d485bf701aa4dee
Alex <423745+alexgenovese@users.noreply.github.com>
2025-02-06 18:16:19 +0100

    update comfy for gradient estimation sampler

--- push-to-docker-hub.sh
+++ push-to-docker-hub.sh
@@ -3,10 +3,21 @@
 # Stop on error
 set -e
 
-# Variables
-IMAGE_NAME="runpod-pod-comfyui"
-TAG="1.1.5"
-DOCKER_HUB_USERNAME="alexgenovese"
+
+# Se esiste un file .env, carica le variabili in esso definite
+if [ -f .env ]; then
+    echo "Caricamento variabili da .env..."
+    # Abilita l'esportazione automatica delle variabili
+    set -o allexport
+    source .env
+    set +o allexport
+fi
+
+# Se una variabile non è stata definita (in .env o altrove) la impostiamo a stringa vuota
+IMAGE_NAME="${IMAGE_NAME:-}"
+TAG="${TAG:-}"
+DOCKER_HUB_USERNAME="${DOCKER_HUB_USERNAME:-}"
+HF_TOKEN="${HF_TOKEN:-}"
 
 # Ensure the script is run with the correct number of arguments
 if [ "$#" -eq 2 ]; then
@@ -19,12 +30,12 @@ if docker info | grep -q "Username: $DOCKER_HUB_USERNAME"; then
     echo "Already logged into Docker Hub as $DOCKER_HUB_USERNAME."
 else
     echo "Logging into Docker Hub..."
-    docker login -u "$DOCKER_HUB_USERNAME"
+    docker login -u "$DOCKER_HUB_USERNAME" -p "$DOCKER_HUB_PASSWORD"
 fi
 
 # Build the Docker image
 echo "Building the Docker image: $IMAGE_NAME:$TAG"
-docker build -t "$DOCKER_HUB_USERNAME/$IMAGE_NAME:$TAG" --platform linux/amd64 .
+docker build --build-arg HUGGINGFACE_ACCESS_TOKEN=$HF_TOKEN -t "$DOCKER_HUB_USERNAME/$IMAGE_NAME:$TAG" --platform linux/amd64 .
 
 # Push the Docker image to Docker Hub
 echo "Pushing the Docker image to Docker Hub"
