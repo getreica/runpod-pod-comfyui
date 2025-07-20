7f224a0bb4279dd0ed998a621910529dd0c32278
Alex Genovese <423745+alexgenovese@users.noreply.github.com>
2025-07-20 16:18:02 +0200

    Update 3-ai-toolkit-on-workspace.sh

--- 3-ai-toolkit-on-workspace.sh
+++ 3-ai-toolkit-on-workspace.sh
@@ -26,6 +26,25 @@ if [[ ! -d /workspace/ai-toolkit ]]; then
 	# when trained using the CLI, the result set is stored in /workspace/LoRas (don't put it in /workspace/ai-toolkit/output because it will create a symlink loop)
 	ln -s /workspace/ai-toolkit/LoRas /workspace/models/loras/ai-toolkit
 
+	# Download and install nvm:
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+	# in lieu of restarting the shell
+	\. "$HOME/.nvm/nvm.sh"
+
+	# Download and install Node.js:
+	nvm install 22
+
+	# Verify the Node.js version:
+	node -v # Should print "v22.17.1".
+	nvm current # Should print "v22.17.1".
+
+	# Verify npm version:
+	npm -v # Should print "10.9.2".
+
+	cd ui
+	npm run build_and_start
+
 else
 	# otherwise – do nothing
 	echo "----- No Installation required. Ai Toolkit is on /workspace/ai-toolkit ------"
